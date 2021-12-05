fec253b8b500146bcdc5d231121a99ed889c76a7
Alex I <ailushyk@gmail.com>
2021-12-05 01:02:38 +0100

    chore: setup linter (#3)
    eslint + prettier

---
+++ packages/eslint-config-xteam-app/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    // indent: ['error', 2],
+    // 'linebreak-style': ['error', 'unix'],
+    // quotes: ['error', 'single'],
+    // semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      // configure Prettier for ESLint
+      {
+        arrowParens: 'avoid',
+        printWidth: 120,
+        singleQuote: true,
+      },
+    ],
+  },
+};
