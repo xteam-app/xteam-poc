fe5c2f98ef97f0fcf266665024f6fb2b4a84ec46
Alex Ilushyk <ailushyk@gmail.com>
2021-12-11 00:53:05 +0100

    add storybook scripts

--- packages/ui/.storybook/web/main.js
+++ packages/ui/.storybook/web/main.js
@@ -1,4 +1,12 @@
 module.exports = {
+  // refs: {
+  //   'design-system': {
+  //     title: "Storybook Design System",
+  //     // url: "https://5ccbc373887ca40020446347-yldsqjoxzb.chromatic.com",
+  //     // expanded: false // optional, true by default
+  //     url: 'http://localhost:7007',
+  //   }
+  // },
   'stories': [
     '../../src/**/*.stories.mdx',
     '../../src/**/*.stories.@(js|jsx|ts|tsx)'
--- packages/ui/.storybook/web/preview.js
+++ packages/ui/.storybook/web/preview.js
@@ -25,7 +25,7 @@ export const parameters = {
         ['View', 'Text', 'Button'],
         'Components',
         'Layouts',
-        'Pages',
+        'Screens',
         'Experiments',
         '*',
         'Example'
