542df574e6ed9bdd7300319deecaa7ec791ad2db
Alex Ilushyk <ailushyk@gmail.com>
2021-12-20 16:05:37 +0100

    setup react navigation

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import { NavigationContainer } from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => App);
+const Bootstrap = () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+};
+AppRegistry.registerComponent(appName, () => Bootstrap);
