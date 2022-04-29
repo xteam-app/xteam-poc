76d3cc8b17e5ddb46b1693a49dd679aea3a9648d
ailiushyk <ailiushyk@cashdirector.com>
2022-04-29 22:27:48 +0200

    create expo app + core ui

---
+++ packages/core/metro.config.js
@@ -0,0 +1,30 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require('path');
+
+// Find the workspace root, this can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(__dirname, '../..');
+const projectRoot = __dirname;
+
+const config = getDefaultConfig(projectRoot);
+
+config.resolver.resolverMainFields = [
+  "sbmodern",
+  ...config.resolver.resolverMainFields,
+];
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: false,
+  },
+});
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [...config.watchFolders, "./.ondevice", workspaceRoot];
+// 2. Let Metro know where to resolve packages, and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = config;
