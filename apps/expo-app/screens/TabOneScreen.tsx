503b903ee15260ce1481ea7aa5d0f4287bac84e7
ailiushyk <ailiushyk@cashdirector.com>
2022-04-29 22:48:34 +0200

    use core within app

--- apps/expo-app/screens/TabOneScreen.tsx
+++ apps/expo-app/screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-
+import {MyButton} from '@xteam-app/core'
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
@@ -8,6 +8,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <MyButton text={'click me'} onPress={() => console.log('click click')} />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
